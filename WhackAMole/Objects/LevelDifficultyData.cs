73bd04e5dfd7f90506654d5ef048849c6a87f3c8
OneLeft <oneleft001@gmail.com>
2025-04-04 18:59:53 -0300

    Added level data dictionaries

---
+++ WhackAMole/Objects/LevelDifficultyData.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhackAMole.Objects
+{
+    static class LevelDifficultyData
+    {
+
+        public static readonly Dictionary<string, int> easyLevelData = new Dictionary<string, int> { 
+            { "Mole", 5 }
+        
+        };
+
+        public static readonly Dictionary<string, int> mediumLevelData = new Dictionary<string, int>
+        {
+            { "Mole", 5 },
+            { "Bomb", 6 }
+        };
+
+        public static readonly Dictionary<string, int> hardLevelData = new Dictionary<string, int>
+        {
+            { "Mole", 5 },
+            { "Bomb", 6 },
+            { "RangerMole", 7 }
+        };
+        
+
+
+
+    }
+
+}
