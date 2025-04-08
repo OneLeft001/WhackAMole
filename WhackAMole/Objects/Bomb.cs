fafb5edba4300f429b8adf5af1c21d63b8dc44e8
OneLeft <oneleft001@gmail.com>
2025-04-07 21:18:31 -0300

    Moved classes into their own file

---
+++ WhackAMole/Objects/Bomb.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace WhackAMole
+{
+    class Bomb : Enemy
+    {
+
+        //private Texture2D _sprite;
+        public override bool IsHarmfull { get { return true; } }
+
+        public Bomb(Texture2D texture) : base(texture)
+        {
+
+            //_sprite = texture;
+
+        }
+
+        public override void updateAbstractClass(GameTime gameTime)
+        {
+
+            if (this._rectangle.Contains(Mouse.GetState().Position)) 
+            {
+
+                //Debug.WriteLine("HOvering over bomb!!");
+
+            }
+
+            base.updateAbstractClass(gameTime);
+        }
+
+
+    }
+
+
+
+}
---
+++ WhackAMole/Objects/RangerMole.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace WhackAMole
+{
+    class RangerMole : Enemy
+    {
+
+        public override bool IsHarmfull { get { return false; } }
+
+        public RangerMole(Texture2D texture) : base(texture) { }
+
+        public override void updateAbstractClass(GameTime gameTime)
+        {
+
+            base.updateAbstractClass(gameTime);
+
+        }
+
+    }
+
+
+
+}
