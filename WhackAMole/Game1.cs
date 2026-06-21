074b1284b212809ac18bcf1920782a8394f66517
OneLeft <oneleft001@gmail.com>
2026-06-21 18:39:49 -0300

    Initial Commit for New Start of Project

--- WhackAMole/Game1.cs
+++ WhackAMole/Game1.cs
@@ -9,35 +9,20 @@ namespace WhackAMole
 {
     public class Game1 : Game
     {
-        private GraphicsDeviceManager _graphics;
+        
         private SpriteBatch _spriteBatch;
 
-        private LevelManager _levelManager;
+        
 
         public Game1()
         {
-
-           _levelManager = LevelManager.Instance;
-
-            
-
-            _graphics = new GraphicsDeviceManager(this);
-            Content.RootDirectory = "Content";
-            IsMouseVisible = true;
-
             
 
         }
 
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
             
-            _graphics.PreferredBackBufferWidth = 1920;
-            _graphics.PreferredBackBufferHeight = 1080;
-            _graphics.ApplyChanges();
-
-            _levelManager.initilize(GraphicsDevice);
 
             
 
@@ -46,25 +31,18 @@ namespace WhackAMole
 
         protected override void LoadContent()
         {
+            
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            // TODO: use this.Content to load your game content here
-
-            //_levelManager.setHoleTexture(Content.Load<Texture2D>("Hole"));
-            _levelManager.loadContent(Content);
         }
 
         protected override void Update(GameTime gameTime)
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
             {
-                Quit();
                 Exit();
             }
-            fullScreen();
-
-            // TODO: Add your update logic here
-            _levelManager.update(gameTime);
+            
 
             base.Update(gameTime);
         }
@@ -73,38 +51,15 @@ namespace WhackAMole
         {
             GraphicsDevice.Clear(Color.DarkGreen);
 
-            // TODO: Add your drawing code here
             _spriteBatch.Begin();
-            _levelManager.draw(_spriteBatch);
+            
             _spriteBatch.End();
 
             base.Draw(gameTime);
         }
 
-        public void Quit()
-        {
-            this.Exit();
-        }
-
-        private void fullScreen()
-        {
-
-            if (Keyboard.GetState().IsKeyDown(Keys.D0))
-            {
-
-                _graphics.IsFullScreen = true;
-
-            }
-            if (Keyboard.GetState().IsKeyDown(Keys.D9))
-            {
-
-                _graphics.IsFullScreen = false;
-
-            }
-
-            _graphics.ApplyChanges();
-
-        }
+        
+        
 
     }
 }
---
+++ WhackAMoleTests/UnitTest1.cs
@@ -0,0 +1,31 @@
+﻿namespace WhackAMoleTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        
+        // Arrange
+        int firstNumber = 5;
+        int secondNumber = 10;
+        
+        // Act
+        bool result = addition(firstNumber, secondNumber);
+
+        // Assert
+        Assert.True(result);
+        Assert.Equal(15, firstNumber + secondNumber);
+
+
+    }
+
+    public bool addition(int a, int b)
+    {
+        if (a + b >= a && a + b >= b)
+        {
+            return true;
+        }
+        return false;
+    }
+}
